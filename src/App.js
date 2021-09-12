361e9eef776e514059d8c97d1f3d290486dcffdd
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-12 11:40:09 +0600

    Custom Theme

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './pages/Create';
 import Notes from './pages/Notes';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fefefe',
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,37 +1,15 @@
-import { Button, Container, makeStyles, Typography } from '@material-ui/core';
+import { Button, Container, Typography } from '@material-ui/core';
 import { KeyboardArrowRight } from '@material-ui/icons';
 import React from 'react';
 
-const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: 'violet',
-    '&:hover': {
-      backgroundColor: 'blue',
-    },
-  },
-  title: {
-    textDecoration: 'underline',
-    marginBottom: 20,
-  },
-});
-
 export default function Create() {
-  const classes = useStyles();
   return (
     <Container>
-      <Typography
-        className={classes.title}
-        variant="h6"
-        component="h2"
-        color="textSecondary"
-        gutterBottom
-      >
+      <Typography variant="h6" component="h2" color="textSecondary" gutterBottom>
         Create a New Note
       </Typography>
 
       <Button
-        className={classes.btn}
         onClick={() => console.log('you clicked me')}
         type="submit"
         color="secondary"
