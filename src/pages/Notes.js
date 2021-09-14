57a6530431b917dc592780d1c24e625a8b48d40b
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 04:21:36 +0600

    JSON Server

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Notes() {
-  return <div>Notes page</div>;
-}
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/notes')
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+  return (
+    <div>
+      {notes.map((note) => (
+        <p key={note.id}>{note.title}</p>
+      ))}
+    </div>
+  );
+};
+
+export default Notes;
