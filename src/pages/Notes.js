509485c7b1870b57da808b6aa7b0048bf71f966e
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 04:45:12 +0600

    Grid System

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,3 +1,4 @@
+import { Container, Grid, Paper } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 
 const Notes = () => {
@@ -9,11 +10,29 @@ const Notes = () => {
       .then((data) => setNotes(data));
   }, []);
   return (
-    <div>
-      {notes.map((note) => (
-        <p key={note.id}>{note.title}</p>
-      ))}
-    </div>
+    <Container>
+      {/* <Grid container>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} md={3}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid> */}
+      <Grid container>
+        {notes.map((note) => (
+          <Grid item xs={12} md={6} lg={4} key={note.id}>
+            <Paper>{note.title}</Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
