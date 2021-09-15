0e02b82f787c3fe7241fe0a39360baa01e46cd1c
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-15 06:20:18 +0600

    Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,41 @@
-import { makeStyles } from '@material-ui/core';
+import { Drawer, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
+
+const drawerWidth = 240;
 
 const useStyle = makeStyles({
   page: {
     background: '#f9f9f9',
     width: '100%',
   },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: 'flex',
+  },
 });
 
 const Layout = ({ children }) => {
   const classes = useStyle();
-  return <div className={classes.page}>{children}</div>;
+  return (
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">RS Note</Typography>
+        </div>
+      </Drawer>
+      <div className={classes.page}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
